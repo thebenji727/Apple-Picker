5a7d73b741bcd81572eedbec722a0b5fe84312bd
thebenji727 <156240876+thebenji727@users.noreply.github.com>
2026-09-08 20:10:42 -0400

    done in class 9-8

--- Apple-Picker/Assets/AppleTree.cs
+++ Apple-Picker/Assets/AppleTree.cs
@@ -13,13 +13,21 @@ public class AppleTree : MonoBehaviour
     public float leftAndRightEdge = 10f;
 
     // RNG to change direction
-    public float changeDirChance = 0.1f;
+    public float changeDirChance = 5f;
 
     // seconds between apple creations
     public float appleDropDelay = 1f;
     void Start()
     {
         // Start dropping apples
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
     }
 
     // Update is called once per frame
@@ -30,6 +38,31 @@ public class AppleTree : MonoBehaviour
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
         //changing direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // moves to the right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+        else if (Random.value < changeDirChance) 
+        {
+            speed *= -1;
+        }
 
-    }
+        void FixedUpdate()
+        {
+            float chance = Random.value;
+            if (chance < changeDirChance) 
+            {
+                Debug.Log("chance = " + chance);
+                Debug.Log("changeDirChance = " + changeDirChance);
+                speed *= -1;
+            }
+
+        }
+
+
+}
 }
